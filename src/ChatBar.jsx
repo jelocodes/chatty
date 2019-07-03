2c01e4a1fceb9f41e942ea5c36093272a12c6d3a
jelocodes <michaelangelo.yambao@gmail.com>
2019-07-03 18:51:05 +0000

    add event listener to log user messages

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+    handleKeyPress = (e) => {
+      if (e.key === 'Enter') {
+        console.log(e.target.value)
+      }
+    }
     render(){
       return (
         <footer className="chatbar">
-          <input className="chatbar-username" placeholder={this.props.currentUser} />
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+            <input className="chatbar-username" placeholder={this.props.currentUser} onKeyUp={this.handleKeyPress} />
+            <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyUp={this.handleKeyPress} />
         </footer>
       );
     }
