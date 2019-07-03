5afd63d029add6ca85b839250d8ded6fbf6605d7
jelocodes <michaelangelo.yambao@gmail.com>
2019-07-03 19:09:13 +0000

    clearing input field upon user submit for better ux

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 class ChatBar extends Component {
     handleKeyPress = (e) => {
       if (e.key === 'Enter') {
-        console.log(e.target.value)
         this.props.handleMessage({id: this.props.generateRandomString(), username: this.props.currentUser, content: e.target.value});
+        e.target.value = '';
       }
     }
     render(){
