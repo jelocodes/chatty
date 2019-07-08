17e8197d8dd7f66b465220806f7f1e4dd75dccd0
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-07-07 22:43:19 -0400

    delete commented out outdated code

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -6,9 +6,6 @@ class ChatBar extends Component {
         if (e.target.matches('.chatbar-message')) {
           this.props.handleMessage({username: this.props.currentUser, content: e.target.value});
         } else if (e.target.matches('.chatbar-username')) {
-          // this.props.handleMessage({data: {name: e.target.value}});
-        //   this.state.socket.send(JSON.stringify({type: "postNotification", content: `${this.state.currentUser.name} changed their name to ${receivedData.data.name}`}));
-        // this.setState({currentUser: receivedData.data});
           this.props.handleMessage({newname: e.target.value, status: `${this.props.currentUser} changed their name to ${e.target.value}`})
         }
         e.target.value = '';
